dd80021eff676f01ae18ec40299cbda7429126de
Zbigniew Machnik <z.machnik@gmail.com>
2024-08-17 01:25:42 +0200

    Fixed opening and saving files on Windows.

--- client-side/src/cpp/main.cpp
+++ client-side/src/cpp/main.cpp
@@ -1,9 +1,19 @@
-#include <QtGui/QGuiApplication>
+#ifdef Q_OS_WASM
+    #include <emscripten.h>
+    #include <QtGui/QGuiApplication>
+#else
+    #include <QtWidgets/QApplication>
+#endif
+
 #include <QtQml/QQmlApplicationEngine>
 
 int main(int argc, char *argv[])
 {
+#ifdef Q_OS_WASM
     QGuiApplication app(argc, argv);
+#else
+    QApplication app(argc, argv);
+#endif
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
